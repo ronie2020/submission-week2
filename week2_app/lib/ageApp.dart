f572df4fb0dc7c0ea0805abea76d85cf191098b1
Roni Rodiana <65641756+ronie2020@users.noreply.github.com>
2020-11-03 14:14:39 +0700

    comit

--- week2_app/lib/ageApp.dart
+++ week2_app/lib/ageApp.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(primarySwatch: Colors.blue[900]),
+    theme: ThemeData(primarySwatch: Colors.blue),
     home: AgeApp())
 );
 
@@ -11,6 +11,28 @@ class AgeApp extends StatefulWidget {
   _AgeAppState createState() => _AgeAppState();
 }
 class _AgeAppState extends State<AgeApp> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  void _showPicker(){
+    showDatePicker(context: context,
+      firstDate: DateTime(1900), 
+      initialDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +44,24 @@ class _AgeAppState extends State<AgeApp> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             OutlineButton(
-              child: Text('Silahkan pilih Tahun lahir Anda'),
-            )
+              padding: EdgeInsets.all(20),
+              child: Text('Silahkan pilih Tahun lahir Anda', style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+              borderSide: BorderSide(color: Colors.blue[900], width: 4.0),
+              color: Colors.white,
+              onPressed: () =>(''),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+            ),
+            Text('Usia Anda Saat ini 30 Tahun', style: TextStyle(
+              fontSize: 20.0, fontWeight: FontWeight.bold,
+            ),),
           ],
         ),
+
       ),
     );
   }
